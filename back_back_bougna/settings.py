b5a9cfb80fbf397e23109180e395af4b5508bb2a
Aubin Koenigssohn <mvogtsinga@gmail.com>
2024-03-21 02:35:01 +0000

    Signup ......

--- back_back_bougna/settings.py
+++ back_back_bougna/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'back_back_bougna.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
